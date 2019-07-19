2be37a34c588daed4a144f34612144f20ddc4f5c
tarzansos <tuminfei1981@gmail.com>
2019-07-19 10:51:58 +0800

    增加workspace

--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,11 @@
-use super::hero::HeroWithId;
+use crate::models::hero::HeroWithId;
 use super::rocket;
 use rocket::http::Status;
 use rocket::local::Client;
 
 #[test]
 fn test_hello() {
-    let client = Client::new(rocket()).unwrap();
+    let client = Client::new(rocket::ignite()).unwrap();
     let mut response = client.get("/").dispatch();
     assert_eq!(response.status(), Status::Ok);
     assert_eq!(response.body_string(), Some("Hello, world!".into()));
